d34d7b8330f98b5e6580fd1352441eb73e28d66b
Hüveyda Tükle <huveydatukle@Huveydas-MacBook-Air-2.local>
2023-09-20 16:46:20 +0100

    connected to db (mongoDB)

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,7 @@
 require("dotenv").config();
 const express = require("express");
+const mongoose = require("mongoose");
+
 const shoppingListsRoutes = require("./routes/shoppingLists");
 
 // express app
@@ -15,7 +17,15 @@ app.use((req, res, next) => {
 //routes
 app.use("/api/shoppingLists", shoppingListsRoutes);
 
-//listen for request
-app.listen(process.env.PORT, () => {
-  console.log("listening on port", process.env.PORT);
-});
+//connect to the db
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    //listen for request
+    app.listen(process.env.PORT, () => {
+      console.log("connected to the db & listening on port", process.env.PORT);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
